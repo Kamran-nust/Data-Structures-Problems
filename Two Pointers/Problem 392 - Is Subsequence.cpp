0ff3d22d1d1e97ec081b60772edb0155c4405cfc
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-14 21:42:09 -0400

    Solution added

--- Two Pointers/Problem 392 - Is Subsequence.cpp
+++ Two Pointers/Problem 392 - Is Subsequence.cpp
@@ -2,3 +2,36 @@
 
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
 // (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        stack<char>st;
+        int j=s.length()-1;
+
+        for (int i=0; i<t.length();i++)
+        {
+            st.push(t[i]);
+        }
+        while (!st.empty() && j>=0)
+        {
+            if(st.top()!=s[j])
+            {
+                st.pop();
+            }
+            else
+            {
+                st.pop();
+                j--;
+            }
+        }
+        if(j==-1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
