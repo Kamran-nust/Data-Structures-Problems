6ed21fca9b8af7775186446f561c6b2cd7e51775
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-18 21:27:52 -0400

    Solution of the Problem added

--- Array-String Problems/Problem 238 - Product of Array Except Self.cpp
+++ Array-String Problems/Problem 238 - Product of Array Except Self.cpp
@@ -3,3 +3,23 @@
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
 // You must write an algorithm that runs in O(n) time and without using the division operation.
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+    int n=nums.size();
+    vector<int>prod(n,1);
+    for (int i=1; i<n; i++)
+    {
+        prod[i]=prod[i-1]*nums[i-1];
+    }
+    int suffix =1;
+    for (int j=(n-2);j>=0;j--)
+    {
+        suffix*=nums[j+1];
+        prod[j]=suffix*prod[j];
+    }
+
+    return prod;
+    }
+};
