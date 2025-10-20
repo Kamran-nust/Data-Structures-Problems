d8a5c09b03d9cc9e2a436fb6322497c92c44ba8e
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-19 20:43:35 -0400

    Better and more optimal Solution added

--- HashMap/Problem 383 - Ransom Note.cpp
+++ HashMap/Problem 383 - Ransom Note.cpp
@@ -5,41 +5,62 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>m;
-        unordered_map<char,int>r;
+unordered_map<char,int> freq;
+
         for (int i=0; i<magazine.length(); i++)
         {
-            if(m.find(magazine[i])==m.end())
-            {
-            m[magazine[i]]=1;
-            }
-            else
-            {
-                m[magazine[i]]++;
-            }
+            freq[magazine[i]]++;
         }
         for (int i=0; i<ransomNote.length(); i++)
         {
-            if(m.find(ransomNote[i])==m.end())
+            freq[ransomNote[i]]--;
+            if(freq[ransomNote[i]]<0)
             {
                 return false;
-            }
-            else
-            {
-                if(r.find(ransomNote[i])==r.end())
-                {
-                    r[ransomNote[i]]=1;
-                }
-                else
-                {
-                    r[ransomNote[i]]++;
-                }
-                if(r[ransomNote[i]]>m[ransomNote[i]])
-                {
-                    return false;
-                }
             }
         }
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+//         unordered_map<char,int>m;
+//         unordered_map<char,int>r;
+//         for (int i=0; i<magazine.length(); i++)
+//         {
+//             if(m.find(magazine[i])==m.end())
+//             {
+//             m[magazine[i]]=1;
+//             }
+//             else
+//             {
+//                 m[magazine[i]]++;
+//             }
+//         }
+//         for (int i=0; i<ransomNote.length(); i++)
+//         {
+//             if(m.find(ransomNote[i])==m.end())
+//             {
+//                 return false;
+//             }
+//             else
+//             {
+//                 if(r.find(ransomNote[i])==r.end())
+//                 {
+//                     r[ransomNote[i]]=1;
+//                 }
+//                 else
+//                 {
+//                     r[ransomNote[i]]++;
+//                 }
+//                 if(r[ransomNote[i]]>m[ransomNote[i]])
+//                 {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
