7ea385767db67902e1c6551cfd528e2024dc7c50
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-19 20:42:10 -0400

    Solution added

--- HashMap/Problem 383 - Ransom Note.cpp
+++ HashMap/Problem 383 - Ransom Note.cpp
@@ -1,3 +1,45 @@
 // Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
 // Each letter in magazine can only be used once in ransomNote.
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        // unordered_map<char,int>m;
+        // unordered_map<char,int>r;
+        // for (int i=0; i<magazine.length(); i++)
+        // {
+        //     if(m.find(magazine[i])==m.end())
+        //     {
+        //     m[magazine[i]]=1;
+        //     }
+        //     else
+        //     {
+        //         m[magazine[i]]++;
+        //     }
+        // }
+        // for (int i=0; i<ransomNote.length(); i++)
+        // {
+        //     if(m.find(ransomNote[i])==m.end())
+        //     {
+        //         return false;
+        //     }
+        //     else
+        //     {
+        //         if(r.find(ransomNote[i])==r.end())
+        //         {
+        //             r[ransomNote[i]]=1;
+        //         }
+        //         else
+        //         {
+        //             r[ransomNote[i]]++;
+        //         }
+        //         if(r[ransomNote[i]]>m[ransomNote[i]])
+        //         {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+    }
+};
