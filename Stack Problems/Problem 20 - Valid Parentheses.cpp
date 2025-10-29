27b8c416510e21567368d125dc48dfdf2ec67b10
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-29 12:53:43 -0400

    Optimal Solution to Problem 20 - "Valid Parentheses" added

--- Stack Problems/Problem 20 - Valid Parentheses.cpp
+++ Stack Problems/Problem 20 - Valid Parentheses.cpp
@@ -4,3 +4,45 @@
     // Open brackets must be closed by the same type of brackets.
     // Open brackets must be closed in the correct order.
     // Every close bracket has a corresponding open bracket of the same type.
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for (int i=0;i<s.length();i++)
+        {
+            
+            if (s[i]== '('||s[i]== '{'||s[i]== '[')
+            {
+                st.push(s[i]);
+            } 
+            else if(s[i]== ')')
+            {
+                if(!st.empty() && st.top()=='(')
+                st.pop();
+                else
+                return false;
+            }
+            else if(s[i]== '}')
+            {
+                if(!st.empty() && st.top()=='{')
+                st.pop();
+                else
+                return false;
+            }
+            else if(s[i]== ']')
+            {
+                if(!st.empty() && st.top()=='[')
+                st.pop();
+                else
+                return false;
+            }
+
+        }
+                
+                if(!st.empty())
+                {return false;}
+        return true;
+    }
+};
