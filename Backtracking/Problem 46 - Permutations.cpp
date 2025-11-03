a8fe2019c60014bf5e48a1dac30d398a74b619e7
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-02 21:19:43 -0500

    Optimal Solution to Problem 46 - "Permutations" added

--- Backtracking/Problem 46 - Permutations.cpp
+++ Backtracking/Problem 46 - Permutations.cpp
@@ -1,1 +1,25 @@
 // Given an array nums of distinct integers, return all the possible . You can return the answer in any order.
+
+class Solution {
+public:
+void perm(vector<int>& nums, vector<vector<int>>& answer, int idx)
+{
+    if(idx==nums.size())
+    {
+        answer.push_back({nums});
+        return;
+    }
+
+    for(int j=idx; j<nums.size(); j++)
+    {
+        swap(nums[idx], nums[j]);
+        perm(nums,answer, idx+1);
+        swap(nums[idx], nums[j]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>answer;
+        perm(nums, answer, 0);
+        return answer;
+    }
+};
