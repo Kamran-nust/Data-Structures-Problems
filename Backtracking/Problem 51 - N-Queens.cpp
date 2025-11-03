44fb8c1d2c771b1e2f875d755a2c4b582fa52a3d
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-03 15:48:50 -0500

    Updated Problem 51 - N-Queens and added the optimal solution

--- Backtracking/Problem 51 - N-Queens.cpp
+++ Backtracking/Problem 51 - N-Queens.cpp
@@ -4,3 +4,58 @@
 // You may return the answer in any order.
 
 // Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+class Solution {
+public:
+    int count=0;
+    bool isSafe (vector<string>&board, int row, int col, int n)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if (board[j][col]=='Q')
+            {
+                return false;
+            }
+        }
+        for(int i= row-1,j=col-1; i>=0 && j>=0; i--, j--)
+        {
+            if (board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        for(int i= row-1,j=col+1; i>=0 && j<n; i--, j++)
+        {
+            if (board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void chess(vector<string>&board, vector<vector<string>>&answer, int row, int n)
+    {
+        if(n==row)
+        {
+            answer.push_back({board});
+            count++;
+            return;
+        }
+        for(int j=0; j<n; j++)
+        {
+            if(isSafe(board, row, j, n))
+            {
+                board[row][j]='Q';
+                chess(board, answer, row+1 , n);
+                board[row][j]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board (n, string(n,'.'));
+        vector<vector<string>>answer;
+        chess(board, answer, 0, n);
+        cout<<count;
+        return answer;
+    }
+};
