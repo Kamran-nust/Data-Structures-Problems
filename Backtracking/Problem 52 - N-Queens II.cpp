8914a1336b09da1a2452b67f3cbfaa6720e90718
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-04 17:50:42 -0500

    Updated Problem 52 - "N-Queens II" and added optimal solution

--- Backtracking/Problem 52 - N-Queens II.cpp
+++ Backtracking/Problem 52 - N-Queens II.cpp
@@ -1,3 +1,59 @@
 // The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 
 // Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+class Solution {
+public:
+    int count=0;
+    bool isSafe(vector<string>&board, int row, int col, int n)
+    {
+        for(int j=0;j<row;j++)
+        {
+            if(board[j][col]=='Q')
+            {
+                return false;
+            }
+        }
+        for (int i= row-1, j=col-1; i>=0 && j>=0; i--, j--)
+        {
+            if(board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        for (int i= row-1, j=col+1; i>=0 && j<n; i--, j++)
+        {
+            if(board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void chess(vector<string>&board, vector<vector<string>>&answer, int row, int n)
+    {
+        if(n==row)
+        {
+            answer.push_back({board});
+            count++;
+            return;
+        }
+        for(int j=0; j<n;j++)
+        {
+            if(isSafe(board, row,j,n))
+            {
+                board[row][j]='Q';
+                chess(board, answer, row+1,n);
+                board[row][j]='.';
+            }
+        }
+    }    
+    int totalNQueens(int n) {
+        vector<vector<string>>answer;
+        vector<string>board (n, string(n,'.'));
+        chess(board, answer, 0, n);
+        return count;
+        
+    }
+};
