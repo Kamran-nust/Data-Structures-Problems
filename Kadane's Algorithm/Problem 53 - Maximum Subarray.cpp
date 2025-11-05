260d452f5cf1077a3150c5bac42a500398f34a71
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-05 14:10:22 -0500

    Added optimal solution to the Problem 53 "Maximum Subarray"

--- Kadane's Algorithm/Problem 53 - Maximum Subarray.cpp
+++ Kadane's Algorithm/Problem 53 - Maximum Subarray.cpp
@@ -1,1 +1,18 @@
 // Given an integer array nums, find the with the largest sum, and return its sum.
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int maxSum=nums[0];
+        int n=nums.size();
+        int currSum=nums[0];
+
+       for (int i=1; i<n;i++)
+        {
+            currSum=max(currSum+nums[i],nums[i]);   
+            maxSum= max(currSum,maxSum);
+        }
+     return maxSum;   
+    }
+};
