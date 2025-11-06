5d6abcf935464c8074f331d15cf0324a87e908de
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-06 10:31:44 -0500

    Optimal Solution for Problem 918 "Maximum Sum Circular Subarray" added

--- Kadane's Algorithm/Problem 918 - Maximum Sum Circular Subarray.cpp
+++ Kadane's Algorithm/Problem 918 - Maximum Sum Circular Subarray.cpp
@@ -5,3 +5,30 @@
 
 // A subarray may only include each element of the fixed buffer nums at most once.
 // Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n= nums.size();
+        int maxSum = INT_MIN;
+        int minSum= INT_MAX;
+        int currMin=0;
+        int currSum= 0;
+        int total=0;
+
+        for (int i=0; i<n;i++)
+        {
+            currSum=max(currSum +nums[i], nums[i]);
+            maxSum=max(currSum, maxSum);
+            currMin= min(currMin+ nums[i], nums[i]);
+            minSum=min(currMin, minSum);
+            total+=nums[i];
+    
+        }
+        if(maxSum<0)
+        {
+        return maxSum;
+        }
+        return max(maxSum, total-minSum);
+    }
+};
