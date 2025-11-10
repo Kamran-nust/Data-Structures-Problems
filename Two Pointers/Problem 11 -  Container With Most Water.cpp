9256ad69147ed6074ffa2323bf53a98353340990
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-10 10:50:04 -0500

    Update Problem 11 -  "Container With Most Water" with Optimal Solution

--- Two Pointers/Problem 11 -  Container With Most Water.cpp
+++ Two Pointers/Problem 11 -  Container With Most Water.cpp
@@ -3,3 +3,30 @@
 // Find two lines that together with the x-axis form a container, such that the container contains the most water.
 // Return the maximum amount of water a container can store.
 // Notice that you may not slant the container.
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int maximum=0;
+        int min_height;
+        int i=0;
+        int j=height.size()-1;
+
+        while(i<j)
+        {
+            min_height=min(height[i], height[j]);
+            // if (height[i]<=height[j])
+            // {
+            // min_height=height[i++];
+            // }
+            // else
+            // {
+            // min_height=height[j--];
+            // }
+            height[i]<=height[j]? i++: j--;
+            maximum= max(maximum, (min_height*(j-i+1)));
+        }
+        return maximum;
+    }
+};
