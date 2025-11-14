509bd4e40cbf21568f623c132dfdf3c7cc691774
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-14 17:32:07 -0500

    Added the optimal solution for Problem 90 - "Subsets II"

--- Backtracking/Problem 90 - Subsets II.cpp
+++ Backtracking/Problem 90 - Subsets II.cpp
@@ -1,3 +1,31 @@
 // Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
 
 // The solution set must not contain duplicate subsets. Return the solution in any order.
+
+class Solution {
+public:
+
+    void uniquesubsets(vector<int>& nums , vector<vector<int>>&answer, vector<int> &ans, int i)
+    {
+        if (i==nums.size())
+        {
+            answer.push_back(ans);
+            return;
+        }  
+        ans.push_back(nums[i]);
+        uniquesubsets(nums ,answer,ans, i+1);
+        while(i<nums.size()-1 && nums[i]==nums[i+1])
+        {
+            i++;
+        }
+        ans.pop_back();
+        uniquesubsets(nums ,answer,ans, i+1);  
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>answer;
+        vector<int> ans;
+        uniquesubsets(nums, answer, ans, 0);
+        return answer;
+    }
+};
