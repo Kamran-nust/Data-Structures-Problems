eea9ad28101bc0a4cc07e84072ad1040d5800cf8
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-21 15:33:08 -0500

    Implemented optimal solution for "diameter for a binary tree" problem

--- Binary Tree/Problem 543 - Diameter of Binary Tree.cpp
+++ Binary Tree/Problem 543 - Diameter of Binary Tree.cpp
@@ -3,3 +3,37 @@
 // The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
 
 // The length of a path between two nodes is represented by the number of edges between them.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    int Height(TreeNode* root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        int left = Height(root->left);
+        int right = Height(root->right);
+        ans= max((left+right), ans);
+
+        return max(left,right)+ 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        Height(root);
+        return ans;
+
+    }
+};
