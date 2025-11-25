57842a6cc80b21306f67a455283690a86604394e
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-25 10:04:33 -0500

    Optimal Solution added for Problem 50 "Pow(x,n)
    Implemented the myPow function to calculate x raised to the power n using binary exponentiation.

--- Math/Problem 50 - Pow(x, n).cpp
+++ Math/Problem 50 - Pow(x, n).cpp
@@ -1,1 +1,34 @@
 // Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        if (n==1) return x;
+        if (x==1) return 1.0;
+        if (n==0) return 1.0;
+        if (x==-1 && n%2==0) return 1.0;
+        if(x==-1 && n%2!=0) return -1.0;
+        double power=1;
+        int remainder;
+        long binary=n;
+        if (n<0)
+        {
+            binary=-binary;
+            x=1/x;
+
+        }
+        
+        while (binary>0)
+        {
+            if(binary%2==1)
+             {
+             power=power*x;
+             }
+            binary=binary/2;
+             x*=x;
+        }
+
+        return power;
+    }
+};
