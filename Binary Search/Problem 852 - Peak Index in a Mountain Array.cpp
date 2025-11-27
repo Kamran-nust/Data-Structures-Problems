b472573be72914f85094800b4c5008455bb96d76
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-27 10:24:13 -0500

    Added solution for Problem 852 " peak Index In Mountain Array"
    Implement binary search to find peak index in mountain array.

--- Binary Search/Problem 852 - Peak Index in a Mountain Array.cpp
+++ Binary Search/Problem 852 - Peak Index in a Mountain Array.cpp
@@ -3,3 +3,33 @@
 // Return the index of the peak element.
 
 // Your task is to solve it in O(log(n)) time complexity.
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int start=1;
+        int end=nums.size()-2;
+        
+        while(start<=end)
+        {
+            int mid=start + ((end-start)/2);
+            cout<<mid;
+            if(nums[mid]>nums[mid-1] && nums[mid]> nums[mid+1])
+            {
+                return mid;
+            }
+            else if (nums[mid]>nums[mid-1])
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+
+        }
+
+        return -1;
+        
+    }
+};
