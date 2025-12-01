5894151186681d3234f7a62cbe980047bb79144a
Muhammad Kamran <kamran_nust@yahoo.com>
2025-11-30 21:51:47 -0500

    Implement optimal solution to find missing and repeated values

--- Array-String Problems/Problem 2965 - Find Missing and Repeated Values.cpp
+++ Array-String Problems/Problem 2965 - Find Missing and Repeated Values.cpp
@@ -3,3 +3,36 @@
 // The task is to find the repeating and missing numbers a and b.
 
 // Return a 0-indexed integer array ans of size 2 where ans[0] equals to a and ans[1] equals to b.
+
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        
+        unordered_set<int>s;
+        int n=grid.size();
+        int expected_sum= ((n*n)*((n*n)+1))/2;
+        int start, repeating,actual_sum,missing;
+        repeating =0;
+        actual_sum=0;
+
+        for (int i=0; i<n; i++)
+        {
+            for(int j=0; j<n;j++)
+            {
+            start=grid[i][j];
+            actual_sum+=start;
+            if (s.find(start)!=s.end())
+            {
+                repeating=start;
+            }
+            s.insert(start);
+            
+            }
+        }
+
+        missing= expected_sum+repeating - actual_sum;
+
+        return {repeating,missing};
+    }
+    
+};
