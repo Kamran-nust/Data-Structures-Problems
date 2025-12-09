2143865619ca95ebe09f4d4d631cb487b64160d9
Muhammad Kamran <kamran_nust@yahoo.com>
2025-12-08 21:40:53 -0500

    Added comments explaining Kadane's Algorithm and subarray properties.

--- Kadane's Algorithm/Problem 53 - Maximum Subarray.cpp
+++ Kadane's Algorithm/Problem 53 - Maximum Subarray.cpp
@@ -4,6 +4,22 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
 
+        //Kadane's Algorithm
+
+        // Subarray is the continuous part of the array.
+        //Total number of subarrays = (n*(n+1))/2
+
+        //  SubarraySum    CurrentElement     action
+        //  Positive       Positive           Extend
+        //  Negative       Positive           New
+        //  Positive       Negative           Extend
+        //  Negative       Negative           New
+        
+
+        // if subarray sum becomes negative reset it to zero 
+        // OR
+        // This means start a new subarray if current element is big alone or extend the existing sub array
+
         int maxSum=nums[0];
         int n=nums.size();
         int currSum=nums[0];
